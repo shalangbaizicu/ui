05b5061a18df692cb00b8dcfff0c8efa2bd9df30
shalangbaizicu <1536618983@qq.com>
2025-07-31 10:22:03 +0800

    Update 砂狼脚本.lua

--- 砂狼脚本.lua
+++ 砂狼脚本.lua
@@ -1,1 +1,2 @@
-966
+loadstring(game:HttpGet("https://raw.githubusercontent.com/shalangbaizicu/ui/main/%E7%A0%82%E7%8B%BC%E5%BC%B9%E7%AA%97.lua"))()
+Httadmin.send("886!", "886", 886, "rbxassetid://124382926972686")
